71c7f1464b5674f3451c87e116d211bc8f4d6b33
ducloc <locdivine@gmail.com>
2024-08-20 14:49:46 +0700

    migrate kapt to ksp

--- app/src/main/java/com/example/ghtk_pokedex/data/model/Pokemon.kt
+++ app/src/main/java/com/example/ghtk_pokedex/data/model/Pokemon.kt
@@ -2,4 +2,7 @@ package com.example.ghtk_pokedex.data.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Pokemon(val name: String, @SerializedName("url") val imageUrl: String)
+data class Pokemon(
+    val name: String,
+    @SerializedName("url") val imageUrl: String
+)
